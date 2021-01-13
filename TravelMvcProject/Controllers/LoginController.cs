f4e02a46693cedc96d7f9e8926eb0d4f42095159
jynyigt <jiyanyiit@yandex.com>
2021-01-13 14:03:55 +0300

    first

---
+++ TravelMvcProject/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using TravelMvcProject.Models.Siniflar;
+
+namespace TravelMvcProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Admin a)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.Kullanici == a.Kullanici && x.Sifre == a.Sifre);
+            if (bilgiler!=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici,false);
+                Session["Kullanici"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
---
+++ TravelMvcProject/Controllers/İletisimController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelMvcProject.Models.Siniflar;
+
+namespace TravelMvcProject.Controllers
+{
+    public class İletisimController : Controller
+    {
+        // GET: İletisim
+
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var deger = c.iletisims.ToList();
+            return View(deger);
+        }
+    }
+}
